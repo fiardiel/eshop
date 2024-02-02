5662a4d5bf7d1b477c5b70496a4758bc93187bec
fiardiel <rafi.ardiel@ui.ac.id>
2024-02-02 10:11:52 +0700

    Initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -0,0 +1,22 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Service
+public class ProductServiceImpl {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public Product create(Product product) {
+        product
+    }
+}
