e8b89908ddbdde30c5129f1008563d3ea37a9e59
fiardiel <rafi.ardiel@ui.ac.id>
2024-02-13 16:01:50 +0700

    cleaned up code

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomeControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomeControllerTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 
 @WebMvcTest(HomeController.class)
-public class HomeControllerTest {
+class HomeControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
