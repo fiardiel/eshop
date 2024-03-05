24dc419efdfa9fbdcadd8a1fffd16e91cedbb5c2
fiardiel <rafi.ardiel@ui.ac.id>
2024-03-05 10:02:07 +0700

    [RED] Added OrderRepository test and also OrderRepository Skeleton

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -60,7 +60,7 @@ public class OrderTest {
     @Test
     void testCreateOrderInvalidStatus() {
         assertThrows(IllegalArgumentException.class, () -> {
-            Order order = new Order("123568123-afaf-013909148",
+            Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
                     this.products, 1708560000L, "Safira Sudrajat", "MEOW");
         });
     }
